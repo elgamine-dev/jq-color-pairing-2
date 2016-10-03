1b4a7df137032cf2755c8a752cc7618356ed36d9
tsague <tsagueymele@yahoo.fr>
2016-10-03 16:18:17 +0200

    exo done

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // good luck !
+
+var inputs = $(".color");
+console.log(inputs);
+//input.color input.color...
+var len = inputs.length;
+console.log(len);
+//10
+for (var i=0; i<len; i++){
+//var colors = inputs[i];
+var color = $(inputs[i]).data('color');
+//console.log(colors);
+console.log(color);
+$(inputs[i]).css("background-color", color);
+}
+
+$('input.color').on('click', function(){
+	var font = $(this).data('color');
+	
+	var appel = checked();
+	console.log(appel);
+	if(appel){
+		$(".main").css('color',font)
+	} else {
+	$('body').css("background-color", font);
+		
+	}
+	
+});
+
+
+
+function checked(){
+return $('#modify-texte').is(':checked');
+
+}
